27e304bdc9b26f1945c7b9014fba62c6cd22096d
Goose <vini.ms01@outlook.com>
2025-03-26 13:52:16 +0100

    clean and play method added

--- pets.py
+++ pets.py
@@ -1,11 +1,12 @@
 class Pet():
-    def __init__(self, name, hp = 10, maxHp = 10, hunger = 0, minHunger = 0, dust = 0):
+    def __init__(self, name, hp = 10, maxHp = 10, hunger = 0, minHunger = 0, dust = 0, minDust = 0):
         self.name = name
         self.hp = hp
         self.maxHp = maxHp
         self.hunger = hunger
         self.minHunger = minHunger
         self.dust = dust
+        self.minDust = minDust
     
     def feed(self):
         self.hunger -= 2
@@ -15,7 +16,18 @@ class Pet():
         if self.hp > self.maxHp:
             self.hp = self.maxHp
         print(f"{self.name} now has {self.hunger} hunger and {self.hp} hp")
-        
+
+    def clean(self):
+        self.dust -= 2
+        if self.dust < self.minDust:
+            self.dust = self.minDust
+        print(f"{self.name} now has {self.dust} dust")
+    
+    def play(self):
+        self.hp += 2
+        if self.hp > self.maxHp:
+            self.hp = self.maxHp
+        print(f"{self.name} now has {self.hp} hp")
 
 class Dog(Pet):
     def __init__(self):
