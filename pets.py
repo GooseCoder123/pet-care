a1dbf000699d03e672121f7dd683808acc27dcd8
Goose <vini.ms01@outlook.com>
2025-03-26 13:47:09 +0100

    feed method added

--- pets.py
+++ pets.py
@@ -1,14 +1,34 @@
 class Pet():
-    pass
+    def __init__(self, name, hp = 10, maxHp = 10, hunger = 0, minHunger = 0, dust = 0):
+        self.name = name
+        self.hp = hp
+        self.maxHp = maxHp
+        self.hunger = hunger
+        self.minHunger = minHunger
+        self.dust = dust
+    
+    def feed(self):
+        self.hunger -= 2
+        self.hp += 1
+        if self.hunger < self.minHunger:
+            self.hunger = self.minHunger
+        if self.hp > self.maxHp:
+            self.hp = self.maxHp
+        print(f"{self.name} now has {self.hunger} hunger and {self.hp} hp")
+        
 
 class Dog(Pet):
-    pass
+    def __init__(self):
+        super().__init__()
 
 class Cat(Pet):
-    pass
+    def __init__(self):
+        super().__init__()
 
 class Rabbit(Pet):
-    pass
+    def __init__(self):
+        super().__init__()
 
 class Parrot(Pet):
-    pass
+    def __init__(self):
+        super().__init__()
