9f993bfdf4849a3f04ce6a3e4fc92dbadaf03310
Goose <vini.ms01@outlook.com>
2025-03-29 13:09:46 +0100

    updated main menu

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ def mainMenu() -> None:
     print("1. Feed")
     print("2. Play")
     print("3. Clean")
-    print("4. Stats")
-    print("5. Change pet")
-    print("6. Exit")
+    print("4. Go walk")
+    print("5. Sleep")
+    print("6. Stats")
+    print("7. Change pet")
+    print("8. Exit")
 
 def userChoice(text: str = "> ") -> str:
     return input(text)
@@ -18,7 +20,7 @@ def changePet():
 choice = 0
 pet = Pet()
 
-while choice != 6:
+while choice != 8:
     mainMenu()
     choice = int(userChoice("Please choose one of the options: "))
 
@@ -29,10 +31,16 @@ while choice != 6:
     elif choice == 3:
         pet.clean()
     elif choice == 4:
-        pet.showStats()
+        pet.goWalk()
     elif choice == 5:
-        changePet()
+        pet.sleep()
     elif choice == 6:
+        pet.showStats()
+    elif choice == 7:
+        changePet()
+    elif choice == 8:
         print("Closing...")
     else:
         print("There's no choice like that, try again")
+
+    pet.update()
